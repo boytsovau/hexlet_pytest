0e7e9fc0f7de4039a11d56e3c94693156879a457
boytsovau <alexbonch@gmail.com>
2023-05-18 16:45:55 +0300

    modified:   hexlet_pytest/__pycache__/example.cpython-311.pyc 	modified:   hexlet_pytest/example.py 	modified:   tests/__pycache__/test_example.cpython-311-pytest-7.3.1.pyc

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,14 @@
+import pytest
+
 def reverse(string):
-    return string[::-2]
+    return string[::-1]
+
+
+
+
+def test_pop_with_empty_stack():
+    stack = [1,2,4]
+    with pytest.raises(IndexError):
+        stack.pop()
+
+test_pop_with_empty_stack()
